442b0d685fbff42e89ae0dda9e683737e28938a0
Moritz Beck <thecrafthalltekkit@web.de>
2018-08-30 17:53:35 +0200

    readme update

--- src/main.js
+++ src/main.js
@@ -3,13 +3,29 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+let app;
+let config = {
+  apiKey: "",
+  authDomain: "sociaskills.firebaseapp.com",
+  databaseURL: "https://sociaskills.firebaseio.com",
+  projectId: "sociaskills",
+  storageBucket: "sociaskills.appspot.com",
+  messagingSenderId: "586033119397"
+};
+
+firebase.initializeApp(config)
+firebase.auth().onAuthStateChanged(function(user) {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      el: '#app',
+      template: '<App/>',
+      components: { App },
+      router
+    })
+  }
+});
