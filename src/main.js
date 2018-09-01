e67d6399e544c443c2adaf3e702f8c81dbf064bf
Moritz Beck <thecrafthalltekkit@web.de>
2018-09-01 10:18:43 +0200

    route error fixed

--- src/main.js
+++ src/main.js
@@ -2,27 +2,24 @@ import Vue from 'vue/dist/vue.js';
 import App from "./App.vue";
 import router from "./router";
 import Argon from "./plugins/argon-kit";
-import firebase from 'firebase';
+import firebase from 'firebase'; 
+import {apiConfig} from "../localConfig";
 
 Vue.config.productionTip = false;
 Vue.use(Argon);
 
-
 let app;
-let config = {
-  apiKey: "",
-  authDomain: "sociaskills.firebaseapp.com",
-  databaseURL: "https://sociaskills.firebaseio.com",
-  projectId: "sociaskills",
-  storageBucket: "sociaskills.appspot.com",
-  messagingSenderId: "586033119397"
-};
+let config;
+if (Vue.config.productionTip == false){
+  config = apiConfig;
+}
 
 
 firebase.initializeApp(config)
 firebase.auth().onAuthStateChanged(function(user) {
   if (!app) {
     /* eslint-disable no-new */
+    console.log(user);
     app = new Vue({
       el: '#app',
       template: '<App/>',
--- src/router.js
+++ src/router.js
@@ -79,7 +79,7 @@ router.beforeEach((to, from, next) => {
   let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
 
   if (requiresAuth && !currentUser) next('login')
-  else if (!requiresAuth && currentUser) next('hello')
+  //else if (!requiresAuth && currentUser) next('hello')
   else next()
 });
 
